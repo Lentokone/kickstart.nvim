ef0034f23d0f7ecc0f1b79a1467f2475f8e08102
Lentokone <olli22012@student.hamk.fi>
2026-08-14 00:44:24 +0300

    Add colorschemes

--- init.lua
+++ init.lua
@@ -410,10 +410,34 @@ do
     },
   }
 
+  -- Colorschemes that looked cool
+  vim.pack.add { gh 'rebelot/kanagawa.nvim' }
+  ---@diagnostic disable-next-line: missing-fields
+  require('kanagawa').setup {
+    styles = {
+      comments = { italic = false }, -- Disable italics in comments
+    },
+  }
+
+  vim.pack.add { { src = 'https://github.com/rose-pine/neovim', name = 'rose-pine' } }
+  ---@diagnostic disable-next-line: missing-fields
+  require('rose-pine').setup {
+    styles = {
+      comments = { italic = false }, -- Disable italics in comments
+    },
+  }
+
+  vim.pack.add { gh 'navarasu/onedark.nvim' }
+  ---@diagnostic disable-next-line: missing-fields
+  require('onedark').setup {
+    styles = {
+      comments = { italic = false }, -- Disable italics in comments
+    },
+  }
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'kanagawa'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
